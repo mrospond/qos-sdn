eeea074c18523672e5f7d7b2e76bcbc03002d4d3
mrospond <marekrospond@gmail.com>
2023-11-21 19:47:06 +0530

    fix: update path

--- scripts/run-mn.sh
+++ scripts/run-mn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo python3 custom_topology.py
+sudo python3 ../custom_topology.py
 
 #sudo mn --controller=remote,ip=127.0.0.1 --mac -i 10.1.1.0/24 --switch=ovsk,protocols=OpenFlow13 --topo=single,4
