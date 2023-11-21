190c66213286fd88333a08c0af4e85d7667e6912
mrospond <marekrospond@gmail.com>
2023-11-22 05:05:30 +0530

    bckp

--- scripts/run-qos.sh
+++ scripts/run-qos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "starting..."
+#ofctl_rest for meters (rest_qos meters not working)
 ryu-manager ryu.app.ofctl_rest ryu.app.rest_qos ryu.app.qos_simple_switch_13 ryu.app.rest_conf_switch
 
 #ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ../ryu-apps/qos_simple_switch_13.py
